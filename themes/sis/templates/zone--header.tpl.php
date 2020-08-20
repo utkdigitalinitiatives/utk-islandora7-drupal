f33e49cdc59f593a383b9edbd9f2fb2fad176297
Mat Jordan <mat@utk.edu>
2020-08-20 14:39:31 -0400

    Fix module invoke error.

--- themes/sis/templates/zone--header.tpl.php
+++ themes/sis/templates/zone--header.tpl.php
@@ -1,4 +1,6 @@
 <?php
 $block = module_invoke('utk_lib_digital_custom', 'block_view', 'utk_lib_digital_custom_header');
-print $block['content'];
+if (array_key_exists('content', $block)) :
+    print $block['content'];
+endif;
 ?>
